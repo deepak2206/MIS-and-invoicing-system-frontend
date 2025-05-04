27e36b699c9698decc87a5f488763fb8cc644481
deepak2206 <suthardeepak658@example.com>
2025-05-04 13:01:41 +0530

    Initial commit for frontend

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://mis-backend-8mrz.onrender.com',
+  baseURL: 'https://mis-backend-8mrz.onrender.com'
 });
+
 
 export default instance;
