4885fd59d843256dcb66db4da09e0c6d16e8d972
deepak2206 <suthardeepak658@example.com>
2025-05-06 12:05:38 +0530

    Initial working frontend commit

--- src/services/chainService.js
+++ src/services/chainService.js
@@ -3,10 +3,14 @@ import axios from 'axios';
 const BASE = import.meta.env.VITE_API_BASE_URL;
 
 export const getChains = () => axios.get(`${BASE}/api/chains`, { withCredentials: true });
-export const getChainsByGroup = (groupName) => axios.get(`${BASE}/api/chains/filter`, {
-  params: { groupName },
-  withCredentials: true
-});
+// export const getChainsByGroup = (groupName) => axios.get(`${BASE}/api/chains/filter`, {
+//   params: { groupName },
+//   withCredentials: true
+// });
+
+export const getChainsByGroup = (groupId) =>
+  axios.get(`/api/chains/group/${groupId}`, { withCredentials: true });
+
 export const deleteChain = (id) => axios.delete(`${BASE}/api/chains/${id}`, { withCredentials: true });
 export const addChain = (data) => 
     axios.post(`${BASE}/api/chains`, data, { withCredentials: true });
